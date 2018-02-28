3122cf8b005a9ed13757afad9c4c15dddf02528b
David G <dgabbe@acm.org>
2018-02-28 17:40:36 -0500

    Updated documentation; ran spell check on documentation; some Roxygen comments are missing.

--- R/btpress-package.R
+++ R/btpress-package.R
@@ -5,7 +5,7 @@
 #' performance and comfort.
 #'
 #' Imagine that you could ride one gear faster or feel less fatigued at the end of
-#' your ride and it cost you nothing. Tuning your bike tire pressue to you, your
+#' your ride and it cost you nothing. Tuning your bike tire pressure to you, your
 #' bike, and your load may do just that. It all depends on wider bike tires with
 #' flexible sidewalls.
 #'
@@ -21,12 +21,12 @@
 #'   racing tires, marketed as having a casing from a tubular tire, used by
 #'   professional racers, or are a folding tire are good candidates. On the
 #'   other hand, puncture resistant tires,
-#'   \href{snow tires}{https://edsasslercoaching.com/product-reviews/nokian-a10-studded-winter-tires/},
+#'   \href{https://edsasslercoaching.com/product-reviews/nokian-a10-studded-winter-tires/}{snow tires},
 #'   and thick touring tires are supported by their sidewalls and do not
 #'   respond to pressure changes very well.
 #'
 #'   The simplest way to calculate your tire pressure is to use the
-#'   \href{companion web app}{http://btp.frame38.com}, which uses this package.
+#'   \href{http://btp.frame38.com}{companion web app}, which uses this package.
 #'
 #'   To calculate your tire pressures execute this code sequence:
 #'   ```{r eval=FALSE}
@@ -42,9 +42,9 @@
 #'   threads from breaking.
 #'
 #'   Let's say your trials indicate a performance improvement and you'd like to
-#'   \href{explore other tire options}{https://janheine.wordpress.com/2017/06/21/choosing-your-tires/},
+#'   \href{https://janheine.wordpress.com/2017/06/21/choosing-your-tires/}{explore other tire options},
 #'    or it's simply time for new tires. A wider tire is a great option to
-#'   \href{consider}{https://janheine.wordpress.com/2013/03/12/how-wide-a-tire-can-i-run/}.
+#'   \href{https://janheine.wordpress.com/2013/03/12/how-wide-a-tire-can-i-run/}{consider}.
 #'   The following code compares 2 setups and builds upon the first example.
 #'   ```{r eval=FALSE}
 #'   new_tires <- bike_tire_pressures() # Supply new tires for the parameters
@@ -72,7 +72,7 @@
 #'   distribution on a level surface.  A bathroom scale placed under one wheel
 #'   and a spacer block the same height as the scale placed under the other
 #'   wheel.  Load up the bike, hop on, and have a helper steady you and read the
-#'   numbers. Flip the bike around and meaure the other wheel.  Divide the front
+#'   numbers. Flip the bike around and measure the other wheel.  Divide the front
 #'   weight by the rear weight and use that for the `front_distribution`
 #'   parameter.
 #'
@@ -93,7 +93,7 @@
 #'   your tires to the calculated pressure plus another 15psi.  If a test ride
 #'   seems harsher or your bike bounces around more, the pressure is too high.
 #'   Repeat, but this time inflate to 15psi less than the calculated pressure.
-#'   If your wheels bottom out or the handling is squishly, the pressure is too
+#'   If your wheels bottom out or the handling is squishy, the pressure is too
 #'   low.  Adjust in increments of 5psi and repeat until satisfied.
 #'
 #' @section Pump Gauge Inaccuracies - Advanced:
@@ -115,39 +115,38 @@
 #'
 #'   \itemize{
 #'     \item
-#'   \href{Science and Bicycles 1: Tires and Pressure}{https://janheine.wordpress.com/2010/10/18/science-and-bicycles-1-tires-and-pressure/}
+#'   \href{https://janheine.wordpress.com/2010/10/18/science-and-bicycles-1-tires-and-pressure/}{Science and Bicycles 1: Tires and Pressure}
 #'      \item
-#'   \href{The Tire Pressure Revolution}{https://janheine.wordpress.com/2015/01/26/the-tire-pressure-revolution/}
+#'   \href{https://janheine.wordpress.com/2015/01/26/the-tire-pressure-revolution/}{The Tire Pressure Revolution}
 #'      \item
-#'   \href{Tire Pressure: Data and Details}{https://janheine.wordpress.com/2015/02/17/tire-pressure-data-and-details/}
-#'      \item \href{Why Wider Tires Corner Better}{https://janheine.wordpress.com/2015/11/19/why-wider-tires-corner-better/}
+#'   \href{https://janheine.wordpress.com/2015/02/17/tire-pressure-data-and-details/}{Tire Pressure: Data and Details}
+#'      \item \href{https://janheine.wordpress.com/2015/11/19/why-wider-tires-corner-better/}{Why Wider Tires Corner Better}
 #'      \item
-#'   \href{Compass Bon Jon Pass 700C x 35 in Top 5 Fastest Tires in the World}{https://janheine.wordpress.com/2018/01/31/one-of-the-5-fastest-tire-in-the-world/}
+#'   \href{https://janheine.wordpress.com/2018/01/31/one-of-the-5-fastest-tire-in-the-world/}{Compass Bon Jon Pass 700C x 35 in Top 5 Fastest Tires in the World}
 #'      \item
-#'   \href{Silca - Road to Roubaix - The Complete Story}{https://silca.cc/blogs/journal/115178628-road-to-roubaix-the-complete-story}
+#'   \href{https://silca.cc/blogs/journal/115178628-road-to-roubaix-the-complete-story}{Silca - Road to Roubaix - The Complete Story}
 #'      \item Part one,
-#'   \href{Tire Size, Pressure, Aero, Comfort, Rolling Resistance and More. Part 1: How We Got to Now}{https://silca.cc/blogs/journal/118397252-tire-size-pressure-aero-comfort-rolling-resistance-and-more-part-1-how-we-got-to-now}, of a five part series.
+#'   \href{https://silca.cc/blogs/journal/118397252-tire-size-pressure-aero-comfort-rolling-resistance-and-more-part-1-how-we-got-to-now}{Tire Size, Pressure, Aero, Comfort, Rolling Resistance and More. Part 1: How We Got to Now}, of a five part series.
 #'     \item The original
-#'    \href{Bicycle Quarterly article}{https://www.compasscycle.com/wp-content/uploads/2015/03/BQTireDrop.pdf}
+#'    \href{https://www.compasscycle.com/wp-content/uploads/2015/03/BQTireDrop.pdf}{Bicycle Quarterly article}
 #'      \item
-#'   \href{Road Tire Pressure For Plus And BLACK Wheels}{https://www.hedcycling.com/blog/road-tire-pressure-for-plus-and-black-wheels/}
+#'   \href{https://www.hedcycling.com/blog/road-tire-pressure-for-plus-and-black-wheels/}{Road Tire Pressure For Plus And BLACK Wheels}
 #'      \item
-#'   \href{Tire Pressure – Stop Guessing And Read The Science}{http://trstriathlon.com/talking-tires-with-joshua-poertner/}
+#'   \href{http://trstriathlon.com/talking-tires-with-joshua-poertner/}{Tire Pressure – Stop Guessing And Read The Science}
 #'   }
 #'
-#' @section Acknowledgements:
+#' @section Acknowledgments:
 #'
 #'   Source for data and pratical application to Jan Heine and Frank Berto
 #'   published in
-#'   \href{Bicycle
-#'    Quarterly}{https://www.compasscycle.com/wp-content/uploads/2015/03/BQTireDrop.pdf}.
+#'   \href{https://www.compasscycle.com/wp-content/uploads/2015/03/BQTireDrop.pdf}{Bicycle Quarterly}.
 #'
 #'   The emperical curve fitting formula from the
-#'   \href{BikeTinker}{http://www.biketinker.com/2010/bike-resources/optimal-tire-pressure-for-bicycles/}.
+#'   \href{http://www.biketinker.com/2010/bike-resources/optimal-tire-pressure-for-bicycles/}{BikeTinker}.
 #'
 #' @section Disclaimer:
 #'
-#'   Biking riding is an inheritly risky activity. Remember the bike racer's
+#'   Biking riding is an inherently risky activity. Remember the bike racer's
 #'   motto: \emph{Falling is a matter of 'when', not 'if'.} You are solely
 #'   responsible for not exceeding any limits including, but not exclusive of
 #'   your abilities, maximum tire inflation pressure, rim inflation pressure,
@@ -158,9 +157,8 @@
 #' @section Full Disclosure:
 #'
 #'   I ride and recommend
-#'   \href{Compass
-#'    Bicycle tires}{https://www.compasscycle.com/product-category/components/tires/} and subscribe to
-#'   \emph{\href{Bicycle Quarterly}{https://www.bikequarterly.com/}}. I received
+#'   \href{https://www.compasscycle.com/product-category/components/tires/}{Compass  Bicycle tires} and subscribe to
+#'   \emph{\href{https://www.bikequarterly.com/}{Bicycle Quarterly}}. I received
 #'   no compensation for this program.
 #'
 #' @docType package
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 
 A program to calculate optimized bicycle tire pressure based on research that found a small sag or droop in the profile of the tire lead to better performance and comfort.
 
-Image that you could ride one gear faster or feel less fatigued at the end of your ride and it cost you nothing. Tuning your bike tire pressue to you, your bike, and your load may do just that. It all depends on wider bike tires with flexible sidewalls.
+Imagine that you could ride one gear faster or feel less fatigued at the end of your ride and it cost you nothing. Tuning your bike tire pressure to you, your bike, and your load may do just that. It all depends on wider bike tires with flexible sidewalls.
 
-Instructions for use and details are found [here](http://blog.frame38.com/btpress/index.html).
+Instructions for use and details are found [here](http://blog.frame38.com/btpress/reference/btpress-package.html). Or you can try the companion [Shiny web app](http://btp.frame38.com).
 
 NB: `create_examples_bikes()` is not working.
